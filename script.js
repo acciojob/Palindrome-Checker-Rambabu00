c7fcbc359ffb0728f2ba8886251e6a62c568590a
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-02-22 08:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	var i=0;
+	var j=str.length-1;
+	while(i<=j){
+		if(str.charAt(i)!=str.charAt(j)){
+			return false
+		}
+	}
+return true;
+
 
 }
 module.exports = palindrome
